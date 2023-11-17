09d040d7be399ce4d1944fe213e4468b5903be4d
JanMaciuk <38810823+JanMaciuk@users.noreply.github.com>
2023-11-17 21:08:47 +0100

    Task 3 complete

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -5,13 +5,20 @@ import scala.collection.mutable.HashMap
 def main(): Unit = {
   println("Hello world!")
   println("Zadanie 1:")
-  println(stirling(5,2));
-  println(memorized_stirling(999,998));
-  println(memorized_stirling(999,998));
-  //todo: call
+  println(stirling(5,2))
+  println(memorized_stirling(99,97))
+  println(memorized_stirling(99,97))
+  println("\nZadanie 2:")
+  val made_memorized_stirling = make_memorize(stirling)
+  println(made_memorized_stirling(99,97))
+  println(made_memorized_stirling(99,97))
+  println("\nZadanie 3:")
+  lazy val number =stirlingAndPrint(99,97)
+  println("This prints before the function is called, because its lazy")
+  println(number) // stirlingAndPrint will only be called now
 }
 
-var stirling_memory: mutable.HashMap[(Int,Int),Int] = new mutable.HashMap();
+var stirling_memory: mutable.HashMap[(Int,Int),Int] = new mutable.HashMap()
 
 
 def stirling (n:Int,m:Int):Int = {
@@ -21,6 +28,12 @@ def stirling (n:Int,m:Int):Int = {
     case (n,m) if n<m => 0
     case (n,m) => stirling(n-1,m-1) + m*stirling(n-1,m)
 }
+def stirlingAndPrint(n:Int,m:Int):Int = {
+  println("Stirling function is called")
+  return stirling(n,m)
+
+}
+
 
 def memorized_stirling(n:Int,m:Int):Int = {
     if (stirling_memory.contains((n,m))) {
@@ -33,17 +46,21 @@ def memorized_stirling(n:Int,m:Int):Int = {
     }
 }
 
-def make_memorize[A,B](fun: A=>B):A=>B = {
-    val memory: mutable.HashMap[A,B] = new mutable.HashMap();
-    def memorized_fun(arg:A):B = {
-        if (memory.contains(arg)) {
-            return memory(arg)
-        }
-        else {
-            val result = fun(arg)
-            memory += ((arg) -> result)
-            return result
-        }
+def make_memorize[A, B](fun: (A, A) => B): (A, A) => B = {
+  val memory: mutable.HashMap[(A, A), B] = new mutable.HashMap()
+
+  // Define the memorized function with two arguments
+  def memorized_fun(arg1: A, arg2: A): B = {
+    val key = (arg1, arg2)
+
+    if (memory.contains(key)) {
+      return memory(key)
+    } else {
+      val result = fun(arg1, arg2)
+      memory += (key -> result)
+      return result
     }
-    return memorized_fun
+  }
+
+  return memorized_fun
 }
