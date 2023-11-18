5410162b4c068581e9995e65f2bc2c31befe4489
JanMaciuk <38810823+JanMaciuk@users.noreply.github.com>
2023-11-18 14:51:57 +0100

    Task 4 implementation in progress

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,6 +1,5 @@
 import scala.collection.mutable
 import scala.collection.mutable.HashMap
-
 @main
 def main(): Unit = {
   println("Hello world!")
@@ -16,11 +15,16 @@ def main(): Unit = {
   lazy val number =stirlingAndPrint(99,97)
   println("This prints before the function is called, because its lazy")
   println(number) // stirlingAndPrint will only be called now
+  println("\nZadanie 4:")
+  println(bell.take(10).toList)
+  println(stream_head(bell))
+  println(stream_tail(bell).take(10).toList)
 }
+
 
 var stirling_memory: mutable.HashMap[(Int,Int),Int] = new mutable.HashMap()
 
-
+// Zadanie 1:
 def stirling (n:Int,m:Int):Int = {
   (n,m) match
     case (n,0) => 0
@@ -46,6 +50,7 @@ def memorized_stirling(n:Int,m:Int):Int = {
     }
 }
 
+//Zadanie 2:
 def make_memorize[A, B](fun: (A, A) => B): (A, A) => B = {
   val memory: mutable.HashMap[(A, A), B] = new mutable.HashMap()
 
@@ -63,4 +68,27 @@ def make_memorize[A, B](fun: (A, A) => B): (A, A) => B = {
   }
 
   return memorized_fun
+}
+
+//Zadanie 4:
+lazy val bell: Stream[Int] = {
+  def loop(n: Int): Stream[Int] = {
+    val stirlingNumbers = (0 to n).map(m => stirling(n, m))
+    stirlingNumbers.sum #:: loop(n + 1)
+  }
+  loop(0)
+}
+
+def stream_head[A](stream: Stream[A]): Option[A] = {
+  stream match {
+    case h #:: _ => Some(h)
+    case _ => None
+  }
+}
+
+def stream_tail[A](stream: Stream[A]): Stream[A] = {
+  stream match {
+    case _ #:: tail => tail
+    case _ => Stream.empty
+  }
 }
